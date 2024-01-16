7907f03107f5776047fd420d1b14c4f0583cf70e
saharfayyad02 <saharfayyad02@gmail.com>
2024-01-16 21:15:31 +0200

    knn done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -25,6 +25,8 @@ def clean_data(df):
     data_frame = data_frame.drop_duplicates()
     return data_frame
 
+#logistic regrestion , random forest, desion tree
+
 def convert_to_binary_encoding(df):
     label_encoder = LabelEncoder()
     categorical_columns = ['Sex', 'ChestPainType', 'FastingBS', 'RestingECG', 'ExerciseAngina', 'ST_Slope']
@@ -32,21 +34,23 @@ def convert_to_binary_encoding(df):
     for column in categorical_columns:
         df[column] = label_encoder.fit_transform(df[column])
 
-def KNN(k,X_train, X_test, y_train, y_test):
-    # Initialize the KNN classifier with the specified k
-    knn_classifier = KNeighborsClassifier(n_neighbors=k)
-    knn_classifier.fit(X_train, y_train)
 
-    # Train the classifier on the training data
+
+def KNN(k, X_train, X_test, y_train, y_test, metric='manhattan'):
+    knn_classifier = KNeighborsClassifier(n_neighbors=k, metric=metric)
+    knn_classifier.fit(X_train, y_train)
     y_pred = knn_classifier.predict(X_test)
 
-    # Evaluate the performance of the classifier
     accuracy = accuracy_score(y_test, y_pred)
-    classification_report_result = classification_report(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-    print(f"Results for k={k}:")
-    print(f"Accuracy: {accuracy}")
-    print("Classification Report:\n", classification_report_result)
+    print(f"Results for k={k} using {metric} distance:")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1-score: {f1:.4f}\n")
 
 if __name__ == '__main__':
     df = read_file("heart.csv")
